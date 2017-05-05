93c39bcdd4def6e10da6d92224f957d94b183173
spencerdodd <spencer.evan.dodd@gmail.com>
2017-05-05 11:31:09 -0400

    some tweaks. added vpn pid to polybar. changed lock screen a bit. updated screens

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/.polybar top &
-polybar -c ~/.config/polybar/.polybar bottom &
+polybar -c ~/.config/polybar/polybar top &
+polybar -c ~/.config/polybar/polybar bottom &
 
 echo "Bars launched ..."
