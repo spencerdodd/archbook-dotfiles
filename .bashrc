8d7492755c96533d21d28e068c593b4c0bc50a6c
spencerdodd <spencer.evan.dodd@gmail.com>
2017-05-11 15:10:08 -0400

    added new ip handling polybar scripts and some aliases for vpn connections

--- .bashrc
+++ .bashrc
@@ -10,3 +10,5 @@ PS1='[\u@\h \W]\$ '
 
 screenfetch
 alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
+alias vpn='sudo openvpn --config /etc/openvpn/client/US\ East.ovpn &'
+alias vpn-down='sudo killall -s SIGKILL openvpn'
--- .config/polybar/wanip.sh
+++ .config/polybar/wanip.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 WANIP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
-if [[ `ip link` == *"tun0"* ]]
+# catching 'dig: couldn't resolve resolver1.open.dns.com' with no internet
+if [[ $WANIP == *"dig"* ]]
 then
-	echo "$WANIP (VPN)"
+	echo "no internet"
+# if tun0 is up, openvpn is running, which should mean we're over VPN
+elif [[ `ip link` == *"tun0"* ]]
+then
+	echo "$WANIP (vpn)"
+# if wifi is up, then we should be connected to wifi
 elif [[ `ip link` == *"wlp2s0"* ]]
 then
-	echo "$WANIP (WIFI)"
+	echo "$WANIP (wifi)"
+# if eth0 is up, we have an ethernet cable and are likely over ethernet
 elif [[ `ip link` == *"eth0"* ]]
 then
-	echo "$WANIP (ETHERNET)"
+	echo "$WANIP (eth)"
+# otherwise, nothing is up besides loopback, so no internet
 else
-	echo "NO INTERNET"
+	echo "no internet"
 fi
